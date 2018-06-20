200d132f4bbbf4503a5d6d42050b7ef9645be85a
Amit Singh <amit_nyc@live.com>
2018-06-20 20:29:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+=begin
+class School
+
+
+
+
+  school = School.new
+
+ROSTER = {}
+  def initialize(roster)
+    @roster = roster
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+
+  def add_student=(student, grade)
+    ROSTER[student] = []
+    ROSTER[student] << grade
+  end
+
+end
+=end
+
+
+class School
+
+
+  def initialize(school)
+    @school = school
+  end
+
+end
