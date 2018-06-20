a843b261ad2db517565ac256718bdc3e6ca6bf7a
Amit Singh <amit_nyc@live.com>
2018-06-20 21:18:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -59,7 +59,7 @@ def grade(grade)
 end
 
 def sort
-  @roster.each do |key, value| #iterate over the instance varibale roster
+  @roster.each do |value| #iterate over the instance varibale roster
     value.sort! #use the sort method to premantly sort the values for the array
   end
 end
